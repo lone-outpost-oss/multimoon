56c8da97c3442316c494796bfc17a6d9673698ef
SHA Miao <shamiao@loneoutpost.cn>
2024-05-30 17:05:42 +0800

    fix: upgrade `zip` to v2.1.0, avoid yanked v1.2.3

--- src/common.rs
+++ src/common.rs
@@ -5,12 +5,15 @@
 pub fn timestamp_from_zipfile(file: zip::read::ZipFile, fallback: i64) -> i64 {
     // TODO: remove `chrome` crate, use `time` crate for local datetime instead
     use chrono::TimeZone;
-    let z = file.last_modified();
-    let (y, mo, d, h, mn, s) = (z.year().into(), z.month().into(), 
-        z.day().into(), z.hour().into(), z.minute().into(), z.second().into());
-    chrono::offset::Local
-        .with_ymd_and_hms(y, mo, d, h, mn, s)
-        .earliest()
-        .map(|t| t.timestamp())
-        .unwrap_or(fallback)
+    if let Some(z) = file.last_modified() {
+        let (y, mo, d, h, mn, s) = (z.year().into(), z.month().into(), 
+            z.day().into(), z.hour().into(), z.minute().into(), z.second().into());
+        chrono::offset::Local
+            .with_ymd_and_hms(y, mo, d, h, mn, s)
+            .earliest()
+            .map(|t| t.timestamp())
+            .unwrap_or(fallback)
+    } else {
+        fallback
+    }
 }
